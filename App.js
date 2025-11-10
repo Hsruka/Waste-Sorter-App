03f75e2eb67c2bb86a6d39621d55366b0ebe90b3
Esther Laosantiponlawut <estherlstplw2003@gmail.com>
2025-11-10 19:30:27 +0700

    อัพเดทโลโก้แอปและสไปรช์สกรีน

--- App.js
+++ App.js
@@ -1,45 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import * as SplashScreen from 'expo-splash-screen';
-import { useCallback, useEffect, useState } from 'react'; 
-import MainNavigator from './MainNavigator'; 
-import { AuthProvider } from './AuthContext'; 
-
-SplashScreen.preventAutoHideAsync();
+import React from "react";
+import { AuthProvider } from "./AuthContext";
+import MainNavigator from "./MainNavigator";
 
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await new Promise(resolve => setTimeout(resolve, 3000));
-
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        setAppIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
-
-  if (!appIsReady) {
-    return null;
-  }
-
   return (
-    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
-      <AuthProvider>
-        <MainNavigator />
-      </AuthProvider>
-    </View>
+    <AuthProvider>
+      <MainNavigator />
+    </AuthProvider>
   );
-};
+}
